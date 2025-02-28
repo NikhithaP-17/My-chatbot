340a8ca88cb19a7c5e50974c462fe278ef23ebc3
NikhithaP-17 <palemnikhitha@gmail.com>
2025-02-28 10:23:33 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -16,7 +16,7 @@ if "messages" not in st.session_state:
     st.session_state.messages=[]
 
 for message in st.session_state.messages:
-    with st.chat_message(message(message["role"])):
+    with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 if prompt := st.chat_input("Say something..."):
